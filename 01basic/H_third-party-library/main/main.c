60e83dffcf4fbcf40bd6f1b41cff8ee39d46692b
Will Han <943007206@qq.com>
2022-02-13 06:57:53 -0800

    ENH: update third-party-library
    as title.

--- 01basic/H_third-party-library/main/main.c
+++ 01basic/H_third-party-library/main/main.c
@@ -1,11 +1,6 @@
 #include "my_printf.h"
-#include <cJson.h>
 
 int main(int argc, char *argv[])
 {
-    cJSON cjson;
-
-    memset(&cjson, 0, sizeof(CJSON));
-
     return my_printf("Hello Cmake!\n");
 }
